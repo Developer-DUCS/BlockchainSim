e83b6bd9984cbddf20d86e30941358dec4342154
Seth <saworkman1@gmail.com>
2021-09-14 00:17:05 -0500

    added vscode settings

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
-app.get('/api/customers', cors(), (req, res) => {
+app.get("/api/customers", cors(), (req, res) => {
   const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
+    { id: 1, firstName: "John", lastName: "Doe" },
+    { id: 2, firstName: "Brad", lastName: "Traversy" },
+    { id: 3, firstName: "Mary", lastName: "Swanson" },
   ];
 
   res.json(customers);
