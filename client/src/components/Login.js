72344ecb1ed9c8b4079c62e2f43959dc9ab86769
bvalencia002 <54635066+bvalencia002@users.noreply.github.com>
2021-09-20 11:44:08 -0500

    Created a login interface

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,48 @@
 import React from "react";
-import { Button } from "@mui/material";
+import { Button, Grid, TextField, Container } from "@mui/material";
+
 
 const Login = () => {
+
   return (
-    <div>
-      <h1>Login</h1>
-      <Button variant="contained">Example Button</Button>
-    </div>
+    <Container maxWidth="xs">
+      <form>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField 
+                  fullWidth 
+                  label="Email" 
+                  name="email" 
+                  size="small" 
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  label="Password"
+                  name="password"
+                  size="small"
+                  type="password"
+                  variant="outlined"
+                />  
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            <Button 
+            color="primary" 
+            fullWidth
+            type="submit"
+            variant="contained">
+              Log in
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </Container>
   );
 };
 
