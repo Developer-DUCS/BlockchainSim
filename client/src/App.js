a0847590f775b113399ca7b20c96b709699008ef
Dawson Holderman <dholderman@drury.edu>
2021-09-24 16:37:26 -0500

    Added LandingPage to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import HomePage from "./components/HomePage";
 import Login from "./components/Login";
 import Error from "./components/Error";
+import LandingPage from "./components/LandingPage";
+import NavBar from "./components/NavBar";
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
@@ -13,6 +15,8 @@ class App extends Component {
         <Switch>
           <Route path="/" component={HomePage} exact />
           <Route path="/login" component={Login} />
+          <Route path="/landing" component={LandingPage} />
+          <Route path="/logout" component={HomePage} />
           <Route component={Error} />
         </Switch>
       </Router>
