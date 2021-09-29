7f794cc61dee2dd9e3b6161b32263e3249213946
Seth <saworkman1@gmail.com>
2021-09-29 11:59:48 -0500

    theme demo (started)

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import LandingPage from "./components/LandingPage";
 import CssBaseline from "@mui/material/CssBaseline";
 import NavBar from "./components/NavBar";
 import SignUp from "./components/SignUp";
+import Demo from "./components/Demo";
 
 const darkTheme = createTheme({
   palette: {
@@ -20,6 +21,24 @@ const darkTheme = createTheme({
 const lightTheme = createTheme({
   palette: {
     mode: "light",
+  },
+});
+
+const testTheme = createTheme({
+  palette: {
+    mode: "light",
+    primary: { main: "#388697", light: "#fff", dark: "#1E2019" },
+    secondary: { main: "#90A583", light: "#fff", dark: "#1E2019" },
+    error: { main: "#BC2C1A", light: "#fff", dark: "#1E2019" },
+    warning: { main: "#942911", light: "#fff", dark: "#1E2019" },
+    info: { main: "#9D8420", light: "#fff", dark: "#1E2019" },
+    success: { main: "#8DAB7F", light: "#98b38b", dark: "#1E2019" },
+    background: { paper: "#FFFCF7", default: "#FFFCF7" },
+    text: {
+      primary: "#011627",
+      secondary: "#011627",
+      disabled: "#011627",
+    },
   },
 });
 
@@ -40,6 +59,7 @@ const App = () => {
           <Route path="/login" component={Login} />
           <Route path="/landing" component={LandingPage} />
           <Route path="/create" component={SignUp} />
+          <Route path="/demo" component={Demo} />
           <Route component={Error} />
         </Switch>
       </Router>
