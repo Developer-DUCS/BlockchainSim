891a0e91de8104931bb66fcd9165d42f2a8d6547
Bryan Valencia <bryanlikescode@gmail.com>
2021-10-31 16:41:35 -0500

    both API calls in api/data.js are working. simply post to the URL to insert into your database.

--- client/src/api/data.js
+++ client/src/api/data.js
@@ -28,7 +28,7 @@ router.post("/block", cors(), (req, res) => {
   const transaction_counter = 5;
   const miner = "whodidit";
   const time_created = new Date().toISOString().slice(0, 19).replace("T", " ");
-  let qry = `INSERT INTO blocks_bryan_valencia_com VALUES ('${hash}', '${headerString}', '${transactionsString}', ${transaction_counter}, '${miner}', '${time_created}');`;
+  let qry = `INSERT INTO blocks_seth_workman_com VALUES ('${hash}', '${headerString}', '${transactionsString}', ${transaction_counter}, '${miner}', '${time_created}');`;
   db.query(qry, (err, result) => {
     if (err) {
       console.log(err);
@@ -40,14 +40,14 @@ router.post("/block", cors(), (req, res) => {
 
 router.post("/simulation", cors(), (req, res) => {
   const sim_id = 1;
-  const email = "bryan_valencia_com";
+  const email = "seth@workman.com";
   const sim_name = "My First Blockchain";
   const sim_shared = {
-    emails: ["sean_lowry_com", "seth_workman_com"],
+    emails: ["sean@lowry.com", "bryan@valencia.com"],
   };
   const sim_description = "The first blockchain simulation I ever created!";
-  const sim_created = Date.now();
-  const sim_modified = Date.now();
+  const sim_created = new Date().toISOString().slice(0, 19).replace("T", " ");
+  const sim_modified = new Date().toISOString().slice(0, 19).replace("T", " ");
   const sim_blocks = {
     1: "0104db27ef0e770ea5b0786880ee0883b13b04eeb7c34acebc77c4e47957ae95",
     2: "081d730172ca30cd21eaf9f2c6eb41d70b6ff64910b6ce959a0505be0ba729de",
@@ -55,8 +55,9 @@ router.post("/simulation", cors(), (req, res) => {
     4: "014ae7f504e6ac8439ad29f8bdafcfed4ec3d74364dbb0ffdc8df8827861439f",
     5: "00452e6e3a9450d6a8366a12db71c771a42ce6e93bf85912ac51d4434e721add",
   };
-  let qry =
-    "INSERT INTO simulation VALUES (sim_id, email, sim_name, sim_shared, sim_description, sim_created, sim_modified, sim_blocks);";
+  const sim_shared_string = JSON.stringify(sim_shared);
+  const sim_blocks_string = JSON.stringify(sim_blocks);
+  let qry = `INSERT INTO simulation (email,sim_name,sim_shared,sim_description,sim_created,sim_modified,sim_blocks) VALUES ('${email}', '${sim_name}', '${sim_shared_string}', '${sim_description}', '${sim_created}', '${sim_modified}', '${sim_blocks_string}');`;
   db.query(qry, (err, result) => {
     if (err) {
       console.log(err);
