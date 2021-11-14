3d275ea00e2db77e0449c20ddb3ebd95d1a5757d
Dawson Holderman <dholderman@drury.edu>
2021-11-13 23:09:57 -0600

    New Dark Theme

--- client/src/js/themes/darkTheme.js
+++ client/src/js/themes/darkTheme.js
@@ -4,12 +4,12 @@ const darkTheme = createTheme({
   mode: "dark",
   palette: {
     primary: { main: "#388697", light: "#3d93a5", dark: "#337989" },
-    secondary: { main: "#DE6B48", light: "#e17859", dark: "#db5e37" },
+    secondary: { main: "#A30000", light: "#e17859", dark: "#A30000" },
     error: { main: "#BC2C1A", light: "#cd301c", dark: "#ab2818" },
     warning: { main: "#5D2E8C", light: "#67339b", dark: "#53297d" },
     info: { main: "#9D8420", light: "#ad9223", dark: "#8d761d" },
     success: { main: "#127E59", light: "#148f65", dark: "#106d4d" },
-    background: { paper: "#022c4d", default: "#01213a" },
+    background: { paper: "#554F69", default: "#202030" },
     text: {
       primary: "#fffcf7",
       secondary: "#fff5e3",
