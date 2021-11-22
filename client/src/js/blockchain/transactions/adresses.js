10680313e378221a43c8a314459d5c8577f841da
slowry <slowry@drury.edu>
2021-11-22 09:19:48 -0600

    added simple addresses

--- client/src/js/blockchain/transactions/adresses.js
+++ client/src/js/blockchain/transactions/adresses.js
@@ -1,7 +1,39 @@
 //import chooseMiner from "..block/miningPool";
 //import createMinerPool from "..block/miningPool";
 
-// import db.conn 
+const express = require("express");
+const app = express();
+const router = express.Router();
+const db = require("../../../../../dbConn");
+const cors = require("cors")
+var richMiners = [] //miners with money (just bitcoin reward initially)
+
+// create an array of miners w/ money from bitcoin reward
+router.get("/getAddress", cors(), (req, res) => {
+        
+    qry = `SELECT miner FROM blocks_sean_lowry_com`; //make this dynamic
+    db.query(qry, (err, res) => {
+        if (err) {
+        console.log(err);
+        } else {
+        for (i=0; i < res.length; i++) {
+
+            richMiners.push(res[i].miner)
+        }
+        console.log("Rich Miners: " + JSON.stringify(richMiners))
+        //res.sendStatus(200);
+        }
+    });
+});
+
+subsidy = 50; //basecoin transaction AKA bitcoin reward AKA subsidy
+addresses = {}
+for (i=0; i < richMiners.length; i++) {
+    addresses.push([richMiners[i], subsidy])
+}    
+console.log(addresses)
+
+
 
 /* 
 TO DO:
@@ -10,3 +42,4 @@ TO DO:
 */ 
 
 
+module.exports = router;
