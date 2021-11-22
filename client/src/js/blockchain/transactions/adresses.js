b84e86275486335a5959c9e2bd2c33fc87949042
slowry <slowry@drury.edu>
2021-11-21 19:45:21 -0600

    added comments to transactions - start of sprint 5 research

--- client/src/js/blockchain/transactions/adresses.js
+++ client/src/js/blockchain/transactions/adresses.js
@@ -1,7 +1,12 @@
+//import chooseMiner from "..block/miningPool";
+//import createMinerPool from "..block/miningPool";
+
+// import db.conn 
 
 /* 
 TO DO:
     1. make a list of all people with money, give them the basecoin transaction
     2. make a fake list of 10 miners, give them all money 
 */ 
+
 
