4bdba753b23691be352b54906a2ee0f5b82e8ee6
Laura Pareja-Prieto <lpareja@drury.edu>
2021-12-01 09:15:09 -0600

    initial posible structure of adress pool

--- client/src/js/blockchain/transactions/adressesPool.js
+++ client/src/js/blockchain/transactions/adressesPool.js
@@ -1,3 +1,16 @@
 import { getMiningPool } from "../block/miningPool";
 
-var miningPool = getMiningPool();
+var miningPool;
+
+//function to get miners for adress pool
+const createAdressPoolHeader = () => {
+  miningPool = getMiningPool();
+};
+
+//
+const add2AdrPool = () => {};
+
+//
+const deleteOfAdrPool = () => {};
+
+const getAdress = () => {};
