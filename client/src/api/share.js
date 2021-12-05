ef76d34f0e02ba4e3ffc9f721c87857cf065d959
Dawson Holderman <dholderman@drury.edu>
2021-12-05 15:15:14 -0600

    New share api

--- client/src/api/share.js
+++ client/src/api/share.js
@@ -8,8 +8,36 @@ app.use(express.json());
 
 router.post("/", cors(), (req, res) => {
   //get email from form
+  let email = req.body.email;
+  if (!email) {
+    res.status(401).json({ error: "Missing username." });
+    return;
+  }
+
   //see if the database has that user
-  // if so add the user's email to the simulation ID gathered in the simulation page
-  //if not error
+  let qry = `select * from user where email = "${email}"`;
+  db.query(qry, (err, rows) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ error: err });
+    }
+    //if not error
+    if (rows.length == 0) {
+      // no users found
+      res.status(400).json({ msg: "No users found" });
+    }
+    // if so add the user's email to the simulation ID gathered in the simulation page
+    (qryNew = "insert into simulation (sim_shared) values ?"), [email];
+    db.query(qryNew, (err) => {
+      // If error, log it to console
+      if (err) {
+        console.log(err);
+      } else {
+        // User created
+        res.sendStatus(201);
+      }
+    });
+  });
+
   //hope that the new user can access the sim
 });
