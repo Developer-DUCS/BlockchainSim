b452f44f38426d9f1ddcf0ad7ed4ec8e75c60722
Laura Pareja-Prieto <lpareja@drury.edu>
2021-12-12 14:34:32 -0600

    comments

--- client/src/js/blockchain/transactions/adressesPool.js
+++ client/src/js/blockchain/transactions/adressesPool.js
@@ -1,11 +1,37 @@
 /*
-    Notes:  - use some kind of hash table?.
-            - add some kind of identificator to the every adress to know the user that posses it.
-            - there can be copies of adresses.
-            - think about possible ledger later on --> adresses and transaction table woul be feeded here? 
+    /*
+    --> ADRESSESPOOL.JS FILE
 
-    Adress structure:
-    [ user, $$$ , parent_block]
+    --> FUNCTIONS:
+      - createAdressPoolHeader()
+          - Inputs:
+            * num_miners: number of people in the mining pool
+          - Outputs:
+            * ----
+      
+      - adressesPool (global object): adress pool that contains valid adresses at certain moment. Adresses get added
+            when created in a transaction and deleted when spent in a transaction
+          - How that table looks like:
+          -------------------------------------------------------------------------------
+          |  u1        | u2         | u3         | u4         | ...      | uX           |
+          -------------------------------------------------------------------------------
+          | [u1,50,0]  |[u2,50,1]   |[u3,50,3]   | [u4,20,4]  | ...      | [ux,50,2]    |
+          | [u1,10,50] |            |[u3,10,120] |            | ...      | [ux,0.5,101] |
+          |            |            |[u3,2,201]  |            | ...      |              |
+          -------------------------------------------------------------------------------
+
+          - adress structure: [ user, $$$, block_parent] --> [u3, 50 , 2]
+              * user: person who owns the adress
+              * $$$: amount of coin that adress has
+              * block_parent: height of the block where the adress was created
+
+    *CONNECTIONS*:
+      - File called from block.js (blockCreator())
+      - File calls: 
+        * chooseMiner() (miningPool.js) --> choose a user from already created pool of miners
+        * coinbaseTransaction() (coinbaseTransaction.js) --> create coin base transaction
+        * singleTransaction() (singleTransaction.js) --> create a transaction between two users given an array of adresses
+        * adressesPool (adressesPool.js) --> dynamic pool with all non spended UTXOs
 */
 
 const adressesPool = [];
