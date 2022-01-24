e45b04c611acbecf061f108bb5ae108982bdcd8a
Seth <saworkman1@gmail.com>
2022-01-24 11:07:08 -0600

    testing completed for users.test.js

--- client/src/tests/unit/api/users.test.js
+++ client/src/tests/unit/api/users.test.js
@@ -1,5 +1,6 @@
 const users = require("../../../../../api/users");
 const assert = require("assert");
+const db = require("../../../../../dbConn");
 
 const request = require("supertest");
 const express = require("express");
@@ -9,11 +10,82 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/", users);
 
+const email = "testonly@test.com";
+const password = "testonly";
+const role = "dev";
+
+// Clear Database before testing (removes test data)
+beforeAll((done) => {
+  let qry = `DELETE FROM user WHERE email='${email}'`;
+  db.query(qry, (err, result) => {
+    if (err) {
+      console.error(err);
+    } else {
+      qry = `DROP TABLE IF EXISTS blocks_testonly_test_com`;
+      db.query(qry, (err, result) => {
+        if (err) {
+          console.error(err);
+        } else {
+          done();
+        }
+      });
+    }
+  });
+});
+
+// Clear Database after testing (removes test data)
+afterAll((done) => {
+  let qry = `DELETE FROM user WHERE email='${email}'`;
+  db.query(qry, (err, result) => {
+    if (err) {
+      console.error(err);
+    } else {
+      qry = `DROP TABLE IF EXISTS blocks_testonly_test_com`;
+      db.query(qry, (err, result) => {
+        if (err) {
+          console.error(err);
+        } else {
+          done();
+        }
+      });
+    }
+  });
+});
+
+// Tests are set up in a linear progression (ex: /register before /login to test)
+test("Route /register works", (done) => {
+  request(app)
+    .post("/register")
+    .type("form")
+    .send({ id: email, pass: password, role: role })
+    .expect(201)
+    .then(() => {
+      done();
+    })
+    .catch((err) => {
+      done(err);
+    });
+});
+
+test("Route /register fails, email already taken", (done) => {
+  request(app)
+    .post("/register")
+    .type("form")
+    .send({ id: email, pass: password, role: role })
+    .expect(409)
+    .then(() => {
+      done();
+    })
+    .catch((err) => {
+      done(err);
+    });
+});
+
 test("Route /login success", (done) => {
   request(app)
     .post("/login")
     .type("form")
-    .send({ email: "testing@testing.com", password: "testing" })
+    .send({ email: email, password: password })
     .expect(200)
     .then((response) => {
       assert(response.body.msg == "user authenticated");
@@ -28,7 +100,7 @@ test("Route /login bad password", (done) => {
   request(app)
     .post("/login")
     .type("form")
-    .send({ email: "testing@testing.com", password: "failing" })
+    .send({ email: email, password: "failing" })
     .expect(401)
     .then(() => {
       done();
@@ -70,7 +142,7 @@ test("Route /auth success", (done) => {
   request(app)
     .post("/login")
     .type("form")
-    .send({ email: "testing@testing.com", password: "testing" })
+    .send({ email: email, password: password })
     .expect(200)
     .then((response) => {
       assert(response.body.msg == "user authenticated");
@@ -91,6 +163,7 @@ test("Route /auth success", (done) => {
       done(err);
     });
 });
+
 test("Route /auth fails", (done) => {
   request(app)
     .post("/auth")
