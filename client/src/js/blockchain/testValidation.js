b06c1efb4f405cebb3696a0e2049ae668246f4cd
Laura Pareja-Prieto <lpareja@drury.edu>
2022-01-29 09:48:04 -0600

    figuring it out signatures and addresses

--- client/src/js/blockchain/testValidation.js
+++ client/src/js/blockchain/testValidation.js
@@ -1,13 +1,25 @@
 var ECKey = require("ec-key");
-var BITCOIN_ELLIPTIC_CURVE = "secp256k1";
+var BITCOIN_ELLIPTIC_CURVE = "secp256k1"; // Posibility of using different elliptic curves? not sure it gives any meaning
 
 const createPublicKeyAndAdress = () => {
   //create public and private key
+  var msg = "trial";
   var randomKey = ECKey.createECKey(BITCOIN_ELLIPTIC_CURVE);
   var privateKey = buf2hex(randomKey.d);
   var publicKey = buf2hex(randomKey.x);
-  console.log("private key: ",privateKey);
+
+  // would the msg be transaction? or adress?
+  var signature = randomKey.createSign("SHA256").update(msg).sign("base64");
+  var verify = randomKey
+    .createVerify("SHA256")
+    .update(msg)
+    .verify(signature, "base64");
+  console.log("private key: ", privateKey);
   console.log("public key: ", publicKey);
+  console.log("signature: ", signature);
+  console.log("varification: ", verify);
+
+  //create address
 };
 
 //turn buffer of Unit8Array to haxdecimal number hash
