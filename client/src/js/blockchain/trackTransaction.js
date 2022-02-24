4df714e1db675cc8f8048ced7c33cbf6827e5a38
Laura Pareja-Prieto <lpareja@drury.edu>
2022-02-24 09:38:32 -0600

    notes on what to do

--- client/src/js/blockchain/trackTransaction.js
+++ client/src/js/blockchain/trackTransaction.js
@@ -4,8 +4,14 @@
 // do I need to call the API of do already have all the block needed?
 
 const trackAddres = (adr, blocks) => {
-    var adrTreeHist = [];
-    var blockHist = [];
+    var adrTreeHist = []; // [ [input, output], [input, output]] --> do we include amout?
+    var blockHist = []; // [ [hash,heigth] , [hash,heigth] ]
+
+    //find first address block appearance
+    // check if it is an input of an output
+    // if output --> get input; which one of them if there is more than one? all?
+    // if input --> find matching output in another block
+    // do I want to store the transaction hash so we show it too?
 
 
     return [adrTreeHist, blockHist]
