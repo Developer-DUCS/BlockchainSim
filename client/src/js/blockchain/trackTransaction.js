de6c2dd4db5811512c94b8b38b73cc43454c1f13
Laura Pareja-Prieto <lpareja@drury.edu>
2022-02-24 09:23:47 -0600

    starting trackAddress

---
+++ client/src/js/blockchain/trackTransaction.js
@@ -0,0 +1,11 @@
+// File to track addresses appearance given an initial address and a block height
+
+// where is it connected
+// do I need to call the API of do already have all the block needed?
+
+const trackAddres = (adr) => {
+
+
+}
+
+module.exports = { trackAddres}
