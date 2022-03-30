545cb478aaa6891d3dd3c5a250e6936f1660ac47
Seth <saworkman1@gmail.com>
2022-03-30 11:15:27 -0500

    got rid of wallet filter > 0

--- js/blockchain/transactions/transactions.js
+++ js/blockchain/transactions/transactions.js
@@ -145,11 +145,6 @@ const createTransactions = (
 
 //select a sender with valid money to create transaction
 const selectSender = (block_height, walletArr, UTXO_Pool) => {
-  // Filter out wallets with no money
-  walletArr = walletArr.filter((wallet) => {
-    return wallet[4] > 0;
-  });
-
   // choose valid UTXO
   var index = 0;
   var utxo = UTXO_Pool[index];
