e50f80d04002bb093921ed7c243333fea7e214a5
Seth <saworkman1@gmail.com>
2022-04-25 18:02:38 -0500

    example

---
+++ client/src/tests/unit/block/merkleTree/createMerkleTree.test.js
@@ -0,0 +1,7 @@
+import createMerkleTree from "../../../../../../js/blockchain/block/merkleTree";
+
+describe("create merkle tree", () => {
+  it("creates a merkle tree", () => {
+    expect(1 == 1).toBe(true);
+  });
+});
